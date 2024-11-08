21d8c3147705edc89c39f190e6ee2c380f3caeeb
Tim Teichmann <teichmanntim@outlook.de>
2024-11-08 21:38:47 +0100

    add maven build

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -xe
-
-mkdir -p build
-
-cd src
-javac -d ../build me/tim/Crypto.java me/tim/Message.java me/tim/client/Client.java me/tim/server/ClientHandler.java me/tim/server/Server.java
